8eda4cdfb978469dee8706ee4568ab67a5f2d3c6
Remco <superschaap11@outlook.com>
2016-12-16 22:45:21 +0100

    Actually use prefixes

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ const Commands = require('./Utils/command_engine').Commands
 const AccessChecker = require('./Utils/access_checker')
 
 bot.Dispatcher.on(Events.MESSAGE_CREATE, (c) => {
-  var cmd = c.message.content.substr(Config.prefix.length).split(' ')[0].toLowerCase()
-  var suffix
-  suffix = c.message.content.substr(Config.prefix.length).split(' ')
-  suffix = suffix.slice(1, suffix.length).join(' ')
-  var msg = c.message
-  if (Commands[cmd]) {
-    AccessChecker.getLevel(msg.member, (level) => {
-      if (level === 0 && Commands[cmd].adminOnly === true || level === 0 && Commands[cmd].modOnly === true) {
-        msg.reply('this command is restricted, and not available to you.')
-        return
-      }
-      if (level === 1 && Commands[cmd].adminOnly === true) {
-        msg.reply('sorry, only admins can use this command.')
-        return
-      }
-      try {
-        Commands[cmd].fn(bot, msg, suffix)
-      } catch (e) {
-        console.error(e)
-        msg.reply('an error occured while proccessing this command, the admins have been alerted, please try again later')
-      }
-    })
+  if (c.message.content.indexOf(Config.prefix) === 0) {
+    var cmd = c.message.content.substr(Config.prefix.length).split(' ')[0].toLowerCase()
+    var suffix
+    suffix = c.message.content.substr(Config.prefix.length).split(' ')
+    suffix = suffix.slice(1, suffix.length).join(' ')
+    var msg = c.message
+    if (Commands[cmd]) {
+      AccessChecker.getLevel(msg.member, (level) => {
+        if (level === 0 && Commands[cmd].adminOnly === true || level === 0 && Commands[cmd].modOnly === true) {
+          msg.reply('this command is restricted, and not available to you.')
+          return
+        }
+        if (level === 1 && Commands[cmd].adminOnly === true) {
+          msg.reply('sorry, only admins can use this command.')
+          return
+        }
+        try {
+          Commands[cmd].fn(bot, msg, suffix)
+        } catch (e) {
+          console.error(e)
+          msg.reply('an error occured while proccessing this command, the admins have been alerted, please try again later')
+        }
+      })
+    }
   }
 })
 
