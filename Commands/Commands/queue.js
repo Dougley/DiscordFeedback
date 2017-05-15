1f424b4e974033f2594146f29e13171a61096b60
Remco <hello@dougley.com>
2017-05-15 21:39:36 +0200

    Queue commands logging

--- Commands/Commands/queue.js
+++ Commands/Commands/queue.js
@@ -2,6 +2,7 @@ var commands = []
 var state = {}
 var checker = require('../../Utils/access_checker')
 var logger = require('../../Utils/error_loggers')
+var genlog = require('../../Utils/generic_logger')
 var config = require('../../config.js')
 var bugsnag = require('bugsnag')
 
@@ -50,6 +51,11 @@ commands.registerVote = {
         if (reaction.id === '302137374920671233') {
           check.getLevel(user.memberOf('268811439588900865'), function (l) {
             if (l > 0) {
+              genlog.log(bot, user, {
+                message: 'Reported a card as inappropriate in the feed',
+                affected: state[msg.id].UvId,
+                result: (state[msg.id].reports === config.discord.reportThreshold) ? 'Report has been sent to admins': undefined
+              })
               state[msg.id].reports++
               if (state[msg.id].reports === config.discord.reportThreshold) {
                 bot.Channels.get(config.discord.feedChannel).sendMessage(`Feedback with ID ${state[msg.id].UvId} (${msg.embeds[0].title}) has been sent off for admin review.`)
@@ -66,6 +72,10 @@ commands.registerVote = {
                 to: 1
               }).then((s) => {
                 if (user !== null) {
+                  genlog.log(bot, user, {
+                    message: 'Feed-voted',
+                    affected: state[msg.id].UvId
+                  })
                   bot.Channels.get(config.discord.feedChannel).sendMessage(`${user.mention}, your vote has been registered.`).then(c => {
                     setTimeout(() => c.delete(), 5000)
                   })
@@ -106,11 +116,21 @@ commands.registerVote = {
       }
       case 'adminReviewDelete': {
         if (reaction.id === '302137375113609219') {
+          genlog.log(bot, user, {
+            message: 'Dismissed a report',
+            affected: state[msg.id].UvId,
+          })
           bot.Channels.find(c => c.name === 'admin-queue').sendMessage(`The report for ${state[msg.id].embed.title} has been dismissed, no action has been taken.`).then(o => {
             setTimeout(() => bot.Messages.deleteMessages([o.id, msg.id], bot.Channels.find(c => c.name === 'admin-queue').id), 5000)
           })
           delete state[msg.id]
         } else if (reaction.id === '302137375092375553') {
+          genlog.log(bot, user, {
+            message: 'Approved a report',
+            affected: state[msg.id].UvId,
+            result: `Card with ID ${state[msg.id].UvId} has been deleted`
+          })
+
           bot.Channels.find(c => c.name === 'admin-queue').sendMessage(`The report for ${state[msg.id].embed.title} has been approved, the card has been deleted from Uservoice.`).then(o => {
             setTimeout(() => bot.Messages.deleteMessages([o.id, msg.id], bot.Channels.find(c => c.name === 'admin-queue').id), 5000)
           })
