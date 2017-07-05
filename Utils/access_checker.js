082a6dabc51fbf929eda82e8decf1e99567e1507
Remco <superschaap11@outlook.com>
2017-07-05 13:52:45 +0200

    pls

--- Utils/access_checker.js
+++ Utils/access_checker.js
@@ -1,12 +1,14 @@
+/* eslint-disable */
+
 const Config = require('../config.js')
 
 exports.getLevel = function (member, callback) {
   for (var role of member.roles) {
     if (Config.discord.Roles.adminRoles.indexOf(role.id) > -1) {
-      return 2
+      return callback(2)
     } else if (Config.discord.Roles.moderatorRoles.indexOf(role.id) > -1) {
-      return 1
+      return callback(1)
     }
   }
-  return 0
+  return callback(0)
 }
