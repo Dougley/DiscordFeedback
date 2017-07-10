2bf8afa40b1403ecfcd9cfdf3955e4db27f3492d
RSNFreud <clans@freud-online.co.uk>
2017-07-11 00:05:29 +0300

    adding a pipe (#55)
    * adding a pipe
    
    * i think i got it?

--- Commands/Commands/utils.js
+++ Commands/Commands/utils.js
@@ -45,7 +45,7 @@ commands.stats = {
         if (today - parseInt(date) <= 172800000) {
           let parsed = moment(parseInt(date)).format("MMM Do YYYY")
           messagesField.push({
-            name: `Messages on ${parsed}`,
+            name: `Messages on ${parsed} | `,
             value: data.messages[date],
             inline: true
           })
@@ -56,7 +56,7 @@ commands.stats = {
         if (today - parseInt(date) <= 172800000) {
           let parsed = moment(parseInt(date)).format("MMM Do YYYY")
           commandsField.push({
-            name: `Commands on ${parsed}`,
+            name: `Commands on ${parsed} | `,
             value: data.messages[date],
             inline: false
           })
