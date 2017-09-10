80507f1a16c2a2c6f9837806195a6d7465cc13ce
Remco <hello@dougley.com>
2017-09-10 22:52:37 +0200

    Off to a good start Wowiee, these things should've been seperate since the start

--- channels.example.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // 'discord channel id': 'uservoice category id'
-  '259027885841580032': '197839'
-}
---
+++ constants.js
@@ -0,0 +1,38 @@
+// WOW THESE THINGS HAVE THEIR OWN FILE NOW
+// THIS SHOULD'VE HAPPEND WHEN I STARTED THIS PROJECT
+// WOW
+
+module.exports = { 
+  UVRegex: /https?:\/\/[\w.]+\/forums\/(\d{6,})(?:-[\w-]+)?\/suggestions\/(\d{7,})(?:-[\w-]*)?/,
+  AutoRole: {
+    enabled: true,
+    thresholds: {
+      // RoleID: EXP
+    },
+    addons: {
+      // RoleID: [{Datapoint: Threshold}]
+    },
+    decay: {
+      enabled: true, // should EXP decay at all?
+      threshold: 7, // how many days should somebody be inactive for their EXP to start decaying?
+      speed: 1.25, // how fast should EXP decay once they've hit the inactivity mark?
+      exponential: true // should decay be exponential?
+    }
+  },
+  UVChannels: {
+    default: false, // false = ignore !sumbit commands in uncategorized channels. supply a category ID to default to that category. leave blank to submit uncategorized, if possible
+    channels: {
+      // ChannelID: UVcategoryID
+    }
+  },
+  DefaultPerms: {
+    users: {
+      '107904023901777920': 10,
+      '110813477156720640': 10
+    },
+    roles: {
+      '268815351360389121': 10,
+      '268815286067527690': 10
+    }
+  }
+}
