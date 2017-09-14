18280af7f0c0381329f24494dfc2c59501fefc46
Remco <hello@dougley.com>
2017-09-14 21:49:17 +0200

    Initial command handler
    Probably won't work as expected, if at all, but it's a rough draft right now

---
+++ src/events/MESSAGE_CREATE.js
@@ -0,0 +1,17 @@
+const AccessCheck = require('../util/access')
+const Commands = require('../commands/index')
+const Config = require('../../config')
+
+module.exports = function (ctx) {
+  if (ctx.message.content.indexOf(Config.Discord.prefix) === 0) {
+    let parts = ctx.message.content.substr(Config.Discord.prefix.length).split(' ')
+    let suffix = parts.slice(1, parts.length).join(' ')
+    let cmd = parts[0].toLowerCase()
+    if (Commands[cmd]) {
+      if (!Commands[cmd].meta.enabledms && ctx.message.isPrivate) return ctx.channel.sendMessage('This command cannot be used in DMs.')
+      AccessCheck.check(ctx.message.member).then(result => {
+        if (result >= Commands[cmd].meta.level) Commands[cmd].fn(ctx.message, suffix)
+      })
+    }
+  }
+}
