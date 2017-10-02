1277331641fa3b13c4adeede2ce33a20c2bb7cdf
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2017-10-02 09:59:04 -0400

    cleanup #2 don't ban me dabbit edition
    mfw I broke everything

--- src/util/getmail.js
+++ src/util/getmail.js
@@ -4,27 +4,23 @@ const Redis = require('../models/redis')
 
 module.exports = {
   getMail: (userid) => {
-    return new Promise((resolve, reject) => {
-      if (Constants.Debugging.enable) return resolve(Constants.Debugging.mocking.email)
-      UV.v1.loginAsOwner().then(client => {
-        return client.get('users/search.json', {
-          guid: userid
-        })
-      }).then(result => {
-        if (!result.users || result.users.length !== 1) return reject(false)
-        else {
-          Redis.set(`email:${userid}`, result.users[0].email)
-          return resolve(result.users[0].email)
-        }
-      }).catch(console.error)
-    })
+    if (Constants.Debugging.enable) return Constants.Debugging.mocking.email
+    UV.v1.loginAsOwner().then(client => {
+      return client.get('users/search.json', {
+        guid: userid
+      })
+    }).then(result => {
+      if (!result.users || result.users.length !== 1) return Promise.reject
+      else {
+        Redis.set(`email:${userid}`, result.users[0].email)
+        return result.users[0].email
+      }
+    }).catch(console.error)
   },
   getMailCached: (userid) => {
-    return new Promise((resolve, reject) => {
-      Redis.get(`email:${userid}`).then(res => {
-        if (res !== null) return resolve(res)
-        else return module.exports.getMail(userid)
-      }).catch(reject)
-    })
+    Redis.get(`email:${userid}`).then(res => {
+      if (res !== null) return res
+      else return module.exports.getMail(userid)
+    }).catch(Promise.reject)
   }
 }
